be6c7a31bf325d4c758ddcd8b121c74497d80819
jasongi <jasongiancono@gmail.com>
2015-07-13 00:49:52 +0800

    added copyright

--- affine.c
+++ affine.c
@@ -2,6 +2,21 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+
+/*  
+    Copyright (C) 2013 Jason Giancono (jasongiancono@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
 void eea(int a, int b, int* gcd, int*xi, int* yi);
 char encrypt(char plaintext, int a, int b);
 char decrypt(char ciphertext, int a, int b);
